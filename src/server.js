5830e08a3a31dd4f03e5fea96fa674c5f9ed4d93
NOCGY <43009070+ongsuwannoo@users.noreply.github.com>
2021-02-22 14:39:27 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hi", (req, res) => {
-  console.log("TEST");
-  res.send("Hello world!");
+  res.send("Hello world1!");
 });
 
 module.exports = app;
